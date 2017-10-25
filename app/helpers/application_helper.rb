db35ac3a18243c8987168f873a95be241c42997a
tsuru <tenriki52577@gmail.com>
2017-10-25 12:32:19 +0000

    Install the devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,5 +10,19 @@ module ApplicationHelper
     end
   end
 
+  def bootstrap_class_for(flash_type)
+    case flash_type
+      when :success
+        "alert-success"   # green 
+      when :error
+        "alert-danger"    # red
+      when :alert
+        "alert-warning"   # yellow
+      when :notice
+        "alert-info"      # blue
+      else
+        flash_type.to_s
+    end
+  end
     
 end
